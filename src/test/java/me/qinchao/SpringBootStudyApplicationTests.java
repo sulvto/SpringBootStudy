ef43d8cf80bf24a6cb72a6a49f2898567e1619aa
sulvto <lpetrel@outlook.com>
2016-05-14 23:32:56 +0800

    build

---
+++ src/test/java/me/qinchao/SpringBootStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package me.qinchao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringBootStudyApplication.class)
+public class SpringBootStudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
