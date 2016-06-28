6411c10a89c584b6f48d317185d0c6a258128b3c
sulvto <lpetrel@outlook.com>
2016-06-28 23:57:52 +0800

    oltu.oauth2

---
+++ src/main/java/me/qinchao/common/Constants.java
@@ -0,0 +1,11 @@
+package me.qinchao.common;
+
+/**
+ * Created by sulvto on 16-6-28.
+ */
+public class Constants {
+    // TODO
+    public final static String INVALID_CLIENT_DESCRIPTION = "";
+    public final static  String LOGIN_USER = "LOGIN_USER";
+
+}
---
+++ src/main/java/me/qinchao/controller/AuthorizeController.java
@@ -0,0 +1,182 @@
+package me.qinchao.controller;
+
+import me.qinchao.common.Constants;
+import me.qinchao.domain.User;
+import me.qinchao.service.OAuthService;
+import org.apache.oltu.oauth2.as.issuer.MD5Generator;
+import org.apache.oltu.oauth2.as.issuer.OAuthIssuer;
+import org.apache.oltu.oauth2.as.issuer.OAuthIssuerImpl;
+import org.apache.oltu.oauth2.as.request.OAuthAuthzRequest;
+import org.apache.oltu.oauth2.as.request.OAuthTokenRequest;
+import org.apache.oltu.oauth2.as.response.OAuthASResponse;
+import org.apache.oltu.oauth2.common.OAuth;
+import org.apache.oltu.oauth2.common.error.OAuthError;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.OAuthResponse;
+import org.apache.oltu.oauth2.common.message.types.GrantType;
+import org.apache.oltu.oauth2.common.message.types.ResponseType;
+import org.apache.oltu.oauth2.common.utils.OAuthUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.security.auth.Subject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by sulvto on 16-6-28.
+ */
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private OAuthService oAuthService;
+
+    @RequestMapping("/authorize")
+    public Object authorize(Model model, HttpServletRequest request)
+            throws URISyntaxException, OAuthSystemException {
+
+        try {
+            //构建OAuth 授权请求
+            OAuthAuthzRequest oauthRequest = new OAuthAuthzRequest(request);
+            //检查传入的客户端id是否正确
+            if (!oAuthService.checkClientId(oauthRequest.getClientId())) {
+                OAuthResponse response = OAuthASResponse
+                        .errorResponse(HttpServletResponse.SC_BAD_REQUEST)
+                        .setError(OAuthError.TokenResponse.INVALID_CLIENT)
+                        .setErrorDescription(Constants.INVALID_CLIENT_DESCRIPTION)
+                        .buildJSONMessage();
+                return new ResponseEntity(
+                        response.getBody(), HttpStatus.valueOf(response.getResponseStatus()));
+            }
+
+            User currentUser = (User) request.getSession().getAttribute(Constants.LOGIN_USER);
+
+            //如果用户没有登录，跳转到登陆页面
+            if (currentUser == null) {
+                return "login";
+            }
+
+            int userId = currentUser.getId();
+
+            //生成授权码
+            String authorizationCode = null;
+            //responseType目前仅支持CODE，另外还有TOKEN
+            String responseType = oauthRequest.getParam(OAuth.OAUTH_RESPONSE_TYPE);
+            if (responseType.equals(ResponseType.CODE.toString())) {
+                OAuthIssuerImpl oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+                authorizationCode = oauthIssuerImpl.authorizationCode();
+                oAuthService.addAuthCode(authorizationCode, userId);
+            }
+            //进行OAuth响应构建
+            OAuthASResponse.OAuthAuthorizationResponseBuilder builder =
+                    OAuthASResponse.authorizationResponse(request,
+                            HttpServletResponse.SC_FOUND);
+            //设置授权码
+            builder.setCode(authorizationCode);
+            //得到到客户端重定向地址
+            String redirectURI = oauthRequest.getParam(OAuth.OAUTH_REDIRECT_URI);
+
+            //构建响应
+            final OAuthResponse response = builder.location(redirectURI).buildQueryMessage();
+            //根据OAuthResponse返回ResponseEntity响应
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(new URI(response.getLocationUri()));
+            return new ResponseEntity(headers, HttpStatus.valueOf(response.getResponseStatus()));
+        } catch (OAuthProblemException e) {
+            //出错处理
+            String redirectUri = e.getRedirectUri();
+            if (OAuthUtils.isEmpty(redirectUri)) {
+                //告诉客户端没有传入redirectUri直接报错
+                return new ResponseEntity(
+                        "OAuth callback url needs to be provided by client!!!", HttpStatus.NOT_FOUND);
+            }
+            //返回错误消息（如?error=）
+            final OAuthResponse response =
+                    OAuthASResponse.errorResponse(HttpServletResponse.SC_FOUND)
+                            .error(e).location(redirectUri).buildQueryMessage();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(new URI(response.getLocationUri()));
+            return new ResponseEntity(headers, HttpStatus.valueOf(response.getResponseStatus()));
+        }
+    }
+
+    @RequestMapping("/accessToken")
+    public HttpEntity token(HttpServletRequest request)
+            throws URISyntaxException, OAuthSystemException {
+        try {
+            //构建OAuth请求
+            OAuthTokenRequest oauthRequest = new OAuthTokenRequest(request);
+
+            //检查提交的客户端id是否正确
+            if (!oAuthService.checkClientId(oauthRequest.getClientId())) {
+                OAuthResponse response = OAuthASResponse
+                        .errorResponse(HttpServletResponse.SC_BAD_REQUEST)
+                        .setError(OAuthError.TokenResponse.INVALID_CLIENT)
+                        .setErrorDescription(Constants.INVALID_CLIENT_DESCRIPTION)
+                        .buildJSONMessage();
+                return new ResponseEntity(
+                        response.getBody(), HttpStatus.valueOf(response.getResponseStatus()));
+            }
+
+            // 检查客户端安全KEY是否正确
+            if (!oAuthService.checkClientSecret(oauthRequest.getClientSecret())) {
+                OAuthResponse response = OAuthASResponse
+                        .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
+                        .setError(OAuthError.TokenResponse.UNAUTHORIZED_CLIENT)
+                        .setErrorDescription(Constants.INVALID_CLIENT_DESCRIPTION)
+                        .buildJSONMessage();
+                return new ResponseEntity(
+                        response.getBody(), HttpStatus.valueOf(response.getResponseStatus()));
+            }
+
+            String authCode = oauthRequest.getParam(OAuth.OAUTH_CODE);
+            // 检查验证类型，此处只检查AUTHORIZATION_CODE类型，其他的还有PASSWORD或REFRESH_TOKEN
+            if (oauthRequest.getParam(OAuth.OAUTH_GRANT_TYPE).equals(
+                    GrantType.AUTHORIZATION_CODE.toString())) {
+                if (!oAuthService.checkAuthCode(authCode)) {
+                    OAuthResponse response = OAuthASResponse
+                            .errorResponse(HttpServletResponse.SC_BAD_REQUEST)
+                            .setError(OAuthError.TokenResponse.INVALID_GRANT)
+                            .setErrorDescription("错误的授权码")
+                            .buildJSONMessage();
+                    return new ResponseEntity(
+                            response.getBody(), HttpStatus.valueOf(response.getResponseStatus()));
+                }
+            }
+
+            //生成Access Token
+            OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+            final String accessToken = oauthIssuerImpl.accessToken();
+            oAuthService.addAccessToken(accessToken,
+                    oAuthService.getUserIdByAuthCode(authCode));
+
+            //生成OAuth响应
+            OAuthResponse response = OAuthASResponse
+                    .tokenResponse(HttpServletResponse.SC_OK)
+                    .setAccessToken(accessToken)
+                    .setExpiresIn(String.valueOf(oAuthService.getExpireIn()))
+                    .buildJSONMessage();
+
+            //根据OAuthResponse生成ResponseEntity
+            return new ResponseEntity(
+                    response.getBody(), HttpStatus.valueOf(response.getResponseStatus()));
+        } catch (OAuthProblemException e) {
+            //构建错误响应
+            OAuthResponse res = OAuthASResponse
+                    .errorResponse(HttpServletResponse.SC_BAD_REQUEST).error(e)
+                    .buildJSONMessage();
+            return new ResponseEntity(res.getBody(), HttpStatus.valueOf(res.getResponseStatus()));
+        }
+
+    }
+}
